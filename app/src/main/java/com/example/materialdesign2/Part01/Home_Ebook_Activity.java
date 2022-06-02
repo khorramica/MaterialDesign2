8fe4ec963c166505e9f6dfc3f9927c4e49f6bcd2
Reza kh <khorami.reza55@gmail.com>
2022-06-02 16:07:54 +0430

    Home design

---
+++ app/src/main/java/com/example/materialdesign2/Part01/Home_Ebook_Activity.java
@@ -0,0 +1,16 @@
+package com.example.materialdesign2.Part01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.materialdesign2.R;
+
+public class Home_Ebook_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_ebook);
+    }
+}
--- app/src/main/java/com/example/materialdesign2/Part01/Login_Ebook_Activity.java
+++ app/src/main/java/com/example/materialdesign2/Part01/Login_Ebook_Activity.java
@@ -1,17 +1,58 @@
 package com.example.materialdesign2.Part01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.materialdesign2.R;
 
 
 public class Login_Ebook_Activity extends AppCompatActivity {
 
+    CardView cardLogin, cardRegister;
+    Button btnLogin, btnRegister;
+    ImageView imgBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_ebook);
+
+        btnLogin = findViewById(R.id.btn_Login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        cardLogin = findViewById(R.id.card_Login);
+        cardRegister = findViewById(R.id.card_Register);
+
+        imgBack = findViewById(R.id.img_Back);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardRegister.setVisibility(View.GONE);
+                cardLogin.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardLogin.setVisibility(View.GONE);
+                cardRegister.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Ebook_Activity.this, Home_Ebook_Activity.class));
+            }
+        });
     }
 }
