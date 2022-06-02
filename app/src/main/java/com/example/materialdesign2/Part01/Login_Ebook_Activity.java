03074e22cd9d2834972f7d0355490ba845c8a3ab
Reza kh <khorami.reza55@gmail.com>
2022-06-02 10:46:37 +0430

    Initial commit

---
+++ app/src/main/java/com/example/materialdesign2/Part01/Login_Ebook_Activity.java
@@ -0,0 +1,17 @@
+package com.example.materialdesign2.Part01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.materialdesign2.R;
+
+
+public class Login_Ebook_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ebook);
+    }
+}
